6b29789d41e70ea72092baa150780890575e9138
ym31433 <ym31433@gmail.com>
2017-03-17 11:04:57 -0500

    regenerated one hidden with sigmoid

--- hotspot/train_dnn_1_hidden_layer.py
+++ hotspot/train_dnn_1_hidden_layer.py
@@ -55,8 +55,8 @@ W_1 = tf.Variable(tf.zeros([NUM_HIDDEN1, NUM_OUT]))
 b_1 = tf.Variable(tf.zeros(NUM_OUT))
 
 # trained outputs
-#h = tf.sigmoid(tf.add(tf.matmul(x, W_0), b_0))
-h = tf.add(tf.matmul(x, W_0), b_0)
+h = tf.sigmoid(tf.add(tf.matmul(x, W_0), b_0))
+#h = tf.add(tf.matmul(x, W_0), b_0)
 y = tf.add(tf.matmul(h, W_1), b_1)
 
 # golden outputs
@@ -145,8 +145,8 @@ W_0_save = np.concatenate((W_0_save, b_0_save[np.newaxis]), axis=0)
 W_1_save = np.concatenate((W_1_save, b_1_save[np.newaxis]), axis=0)
 W_0_save = np.reshape(W_0_save.T, [-1, 1])
 W_1_save = np.reshape(W_1_save.T, [-1, 1])
-np.savetxt("nn_config/one_hidden/weights.txt", np.append(W_0_save, W_1_save), delimiter=",")
-np.savetxt("data/train.data/train_result/one_hidden.txt", y_save, delimiter=",")
+np.savetxt("nn_config/one_hidden_sigmoid/weights.txt", np.append(W_0_save, W_1_save), delimiter=",")
+np.savetxt("data/train.data/train_result/one_hidden_sigmoid.txt", y_save, delimiter=",")
 '''
 W_0_reshape = tf.reshape(tf.transpose(W_0), [-1, 1])
 W_1_reshape = tf.reshape(tf.transpose(W_1), [-1, 1])
